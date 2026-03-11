64178f74c2a455e012c8dc1cdcd56e7b34ce053b
mhdfahim55 <mhdfahim55@gmail.com>
2026-03-11 11:24:11 +0530

    concatination

---
+++ concatination.js
@@ -0,0 +1,8 @@
+var company = "corpus datahub"
+var place = "calicut"
+
+// our company is corpus datahub , calicut
+// + , ''
+
+console.log("Our company is "+company+" , "+place);
+console.log(`Our company is ${company} , ${place}`);
