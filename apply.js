2bf17a947f285eddd6abad6ea2472d14b1e7bf6c
mhdfahim55 <mhdfahim55@gmail.com>
2026-03-12 11:30:50 +0530

    apply

---
+++ apply.js
@@ -0,0 +1,9 @@
+let person = {
+    name : "fathima",
+    greet: function(){
+        return this.name;
+    }
+}
+
+    console.log(person.greet());
+    
--- findmissing.js
+++ findmissing.js
@@ -1,5 +1,14 @@
 const arr = [1, 2, 4, 5];
 
-const missing = 15 - arr.reduce((a, b) => a + b, 0);
+const n = 5;
+
+// sum of 1..5
+const totalSum = Array.from({length: n}, (_, i) => i + 1)
+                      .reduce((acc, num) => acc + num, 0);
+
+// sum of array
+const arrSum = arr.reduce((acc, num) => acc + num, 0);
+
+const missing = totalSum - arrSum;
 
 console.log(missing);
