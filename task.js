aba06576f3a12a78488dbe2f2cd34bfc34807ce9
mhdfahim55 <mhdfahim55@gmail.com>
2026-03-13 11:07:35 +0530

    every

--- task.js
+++ task.js
@@ -1,3 +1,3 @@
-let nums = [10, 20, 30, 40, 50];
-let sum = nums.some(num => num > 25);
-console.log(sum); // Output: true
+let nums = [1, 2, 3, 4, 5];
+nums.every(num => num > 0) // true
+console.log(nums.every(num=>num>0));
