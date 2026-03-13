6b6d6ec24872b0fe8ba51ca77d165a20027af8f4
mhdfahim55 <mhdfahim55@gmail.com>
2026-03-13 11:14:54 +0530

    square

--- task.js
+++ task.js
@@ -1,3 +1,6 @@
-let nums = [10,15,20,25,30];
-console.log(nums.find(v=>v>20));
-// find a first number greater than 20
+function sqr(nums) {
+    return nums*nums;
+}
+console.log(sqr(10)
+);
+// find square of a number using function expression
