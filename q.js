de4572aedaa867f2bb0caf64191d7b72585d9d1f
mhdfahim55 <mhdfahim55@gmail.com>
2026-03-16 11:33:48 +0530

    operations

--- q.js
+++ q.js
@@ -1,43 +1,19 @@
-function month(num){
-    switch(num){
-        case 1:
-            return 31;
-            break;
-        case 2:
-            return 28;
-            break;
-        case 3:
-            return 31;
-            break;
-        case 4:
-            return 30;
-            break;
-        case 5:
-            return 31;
-            break;
-        case 6:
-            return 30;
-            break;
-        case 7:
-            return 31;
-            break;
-        case 8:
-            return 31;
-            break;
-        case 9:
-            return 30;
-            break;
-        case 10:
-            return 31;
-            break;
-        case 11:
-            return 30;
-            break;
-        case 12:
-            return 31;
-            break;
+function operations(a,b,operator){
+    switch(operator){
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
         default:
-            return "Invalid month number";
+            return "Invalid operator";
     }
 }
-console.log(month(5));
+console.log(operations(5, 3, '+'));
+console.log(operations(5, 3, '-'));
+console.log(operations(5, 3, '*'));
+console.log(operations(5, 3, '/'));
+console.log(operations(5, 3, '%'));
