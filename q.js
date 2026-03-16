8d3b80bc285469951038904a5ec51250aa43eaea
mhdfahim55 <mhdfahim55@gmail.com>
2026-03-16 11:44:20 +0530

    multiplication

--- q.js
+++ q.js
@@ -1,2 +1,7 @@
-for (let i = 1; i <= 10; i++) 
-    console.log(i);
+function multiplication(num){
+    for(let i=1; i<=10; i++){
+        console.log(`${num} x ${i} = ${num * i}`);
+        
+    }
+}
+multiplication(5);
